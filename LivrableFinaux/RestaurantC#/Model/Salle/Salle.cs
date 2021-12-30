a06cff9e43055532d4a0e2076639b4015df9378a
237anthony <58189003+237anthony@users.noreply.github.com>
2021-12-31 00:18:06 +0100

    Salle Model
    création du model pour l'affichage et la disposition des élements du restaurant

---
+++ LivrableFinaux/RestaurantC#/Model/Salle/Salle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProjectResto.Models.Salle
+{
+    [NotMapped]
+    class Salle
+    {
+        public int NombreRangeParSecteur
+        {
+            get; set;
+        }
+
+        //Le nombre de carrés dans la salle
+        public int NombreSecteur{ get; set; }
+
+        //Le nombre de tables par rang
+        public int NombreTableParRang { get; set; }
+    }
+}
