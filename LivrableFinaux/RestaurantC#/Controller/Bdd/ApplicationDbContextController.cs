6d4e9d841c42831f7f2efc04e7d36820d22c0286
TAVICHE <tavichebouanga1@gmail.com>
2021-12-31 12:47:14 +0100

    bdd du contrôleur et du modèle

---
+++ LivrableFinaux/RestaurantC#/Controller/Bdd/ApplicationDbContextController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProjectResto.Controllers
+{
+    using ProjectResto.Models.BDD;
+    class ApplicationDbContextController
+    {
+        private readonly ApplicationDbContext _context;
+        public ApplicationDbContextController()
+        {
+            var contextOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Test")
+                .Options;
+
+            _context = new ApplicationDbContext(contextOptions);
+        }
+
+    }
+}
---
+++ LivrableFinaux/RestaurantC#/Model/Bdd/DBSQLServerUtils.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace ProjectResto.Models.BDD
+{
+    class DBSQLServerUtils
+    {
+        public static SqlConnection GetDBConnection(string datasource, string database, string username, string password)
+        {
+            //
+            // Data Source=TRAN-VMWARE\SQLEXPRESS;Initial Catalog=simplehr;Persist Security Info=True;User ID=sa;Password=12345
+            //
+            string connString = @"Data Source=" + datasource + ";Initial Catalog="
+                        + database + ";Persist Security Info=True;User ID=" + username + ";Password=" + password;
+            /*
+            String source = "Data Source = (LocalDB)\\MSSQLLocalDB; Initial Catalog = " +
+                "C:\\USERS\\HP\\DOCUMENTS\\PROJET MONOGAME\\PROJECTRESTO\\MODELS\\DATABASE.MDF; " +
+                "Integrated Security = True; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; ApplicationIntent = ReadWrite; MultiSubnetFailover = False";
+
+            String source1 = "Data Source = " + datasource + "; Initial Catalog = " + database +
+            "Integrated Security = True; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; ApplicationIntent = ReadWrite; MultiSubnetFailover = False";
+            */
+            SqlConnection conn = new SqlConnection(connString);
+
+            return conn;
+        }
+    }
+}
